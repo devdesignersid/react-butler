4645be8bef3bd06bad6b057d70bd9b7aa42aff1c
Siddharth <siddharth@Siddharths-MacBook-Pro.local>
2021-12-22 14:04:11 +0530

    chore: added .svg file mocks for testing

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
     '^.+\\.(t|j)sx?$': '@swc/jest'
   },
   moduleNameMapper: {
+    '\\.svg': '<rootDir>/app/tests/__mocks__/svg.ts',
     '\\.(woff|ttf|eot|svg)$': '<rootDir>/app/tests/__mocks__/fileMock.ts',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/app/tests/__mocks__/fileMock.ts',
